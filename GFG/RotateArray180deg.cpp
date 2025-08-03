c9d7539494530943a04afa0fdd98b30bed964546
Sayantan <sayantanhalder43@gmail.com>
2025-08-04 00:36:35 +0530

    first commit

---
+++ GFG/RotateArray180deg.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // since number of row=col I am taking n
+    int n;
+    cout << "Enter the value of n(row=col): ";
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "The elements of the array are: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // column reverse
+    for (int j = 0; j < n; j++)
+    {
+        int start = 0, end = n - 1;
+        while (start < end)
+        {
+            swap(arr[start][j], arr[end][j]);
+            start++;
+            end--;
+        }
+    }
+
+    // row reverse
+    for (int i = 0; i < n; i++)
+    {
+        int start = 0, end = n - 1;
+        while (start < end)
+        {
+            swap(arr[i][start], arr[i][end]);
+            start++;
+            end--;
+        }
+    }
+
+    cout << "The array after rotating 180deg is: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
--- GFG/tempCodeRunnerFile.cpp
+++ GFG/tempCodeRunnerFile.cpp
@@ -1,20 +1,2 @@
-int n;
 
-    cout << "Enter the size of the array: ";
-    cin >> n;
-
-    int arr[n];
-
-    cout << "Enter the elements of the array: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    cout << "The original array is: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-
-    cout << endl;
+        while (start < end)
