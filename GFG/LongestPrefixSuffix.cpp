25af561d4ed539e8850f4067fd7a7c73a7cf0c02
Sayantan <sayantanhalder43@gmail.com>
2025-08-15 23:51:08 +0530

    Added comment for better understanding

--- GFG/LongestPrefixSuffix.cpp
+++ GFG/LongestPrefixSuffix.cpp
@@ -7,6 +7,20 @@
 // Input : s = "abab" Output : 2 Explanation : The string "ab" is the longest prefix and suffix.
 
 // Apporach - KMP Method
+
+/*
+Explanation -
+1.The KMP Algorithmis a string pattern matching algorithm that efficiently finds all occurrences of a pattern in
+a given text.It uses method of creating an LPS array for the pattern which tells us how many characters can be skipped
+ when a mismatch happens....
+2.Here we use KMP method to solve the problem within O(n) Time complexity
+3.when the prefix character = suffix character we increase the size of the current substring(prefix = suffix) by prefix+1...
+and incrementing prefix and suffix ;
+4.when characters mismatch , we have 2 condition when the prefix is pointing on the first index and when its not pointing in the first index
+5.when prefix is pointing in the first index its of no use to go backwards to find matching characters as there are not characters i.e we assign the suffix to 0
+6.when prefix is not pointing in the first index we point prefix to the index which is the "value assign of the previous element of prefix"
+7.after traversing to the end of the text we return the last value assigned
+*/
 #include <iostream>
 #include <vector>
 using namespace std;
