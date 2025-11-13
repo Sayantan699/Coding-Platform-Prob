1e9e7f020edc535d7946b7e7b95d2965ad02218d
Sayantan699 <sayantanhalder43@gmail.com>
2025-11-13 16:30:27 +0530

    first commit

---
+++ GFG/DiagonalTreeEasysol.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; 
+
+class Solution {
+  public:
+    
+    
+    vector<int> diagonal(Node *root) {
+        // code here
+        queue<Node*> q;
+        vector<int> ans;
+        
+        q.push(root);
+        
+        while(!q.empty()){
+            Node* temp = q.front();
+            q.pop();
+            
+            while(temp){
+                if(temp -> left) q.push(temp -> left);
+            ans.push_back(temp -> data);
+            temp = temp -> right;
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+    return 0;
+}
